db07ee9a674bf91c229593d5470f96c04ef0d359
Vira Rodionova <bepa.rdnv@gmail.com>
2016-04-08 15:29:06 +0300

    +Connection Class

---
+++ App_Code/ConnectionClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ConnectionClass
+/// </summary>
+public static class ConnectionClass
+{
+    private static SqlConnection conn;
+    private static SqlCommand command;
+
+    static ConnectionClass()
+    {
+        string connectString = ConfigurationManager.ConnectionStrings["Tickets_DBConnectionString"].ToString();
+        conn = new SqlConnection(connectString);
+        command = new SqlCommand("", conn);
+    }
+
+    public static void InsertPlaces(int id, int p_num, int c_id)
+    {
+        string query = string.Format("INSERT INTO places VALUES ('{0}', '{1}', '{2}', '{3}')", id, c_id, p_num, true);
+        command.CommandText = query;
+
+        try
+        {
+            conn.Open();
+            command.ExecuteNonQuery();
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+}
---
+++ Pages/Home.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Home.aspx.cs" Inherits="Pages_Home" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+</asp:Content>
+
